16e0c1f56ae9800e127cb1383d73248e29acb513
Tigrou Ind <tigrou.ind@gmail.com>
2024-04-08 10:58:51 +0200

    Initial commit

---
+++ FIR/Program.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace FIR
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+			float[] weight;
+			if (File.Exists("weights.txt") && new FileInfo("weights.txt").Length > 0)
+			{
+				weight = File.ReadAllLines("weights.txt")
+					.Select(x => float.Parse(x.Replace(",", "."), CultureInfo.InvariantCulture))
+					.ToArray();
+			}
+			else
+			{
+				weight = GetWeights(21).ToArray();
+			}
+
+			var lines = File.ReadAllLines("input.txt").Select(x => x.Split('\t', ' ')).ToArray();
+			Thread.CurrentThread.CurrentCulture = new CultureInfo(lines.SelectMany(x => x).Any(x => x.Contains(",")) ? "fr-FR" : "en-US");
+
+			var input = lines.Skip(lines[0].Any(x => Regex.IsMatch(x, @"[^0-9,.\s]")) ? 1 : 0)
+				.Select(x => x.Select(y => (y == string.Empty || y == "#N/A") ? 0.0f : float.Parse(y)).ToArray()).ToArray();
+
+			float[][] output = new float[lines.Length].Select(x => new float[input[0].Length]).ToArray();
+			for (int j = 0; j < input.Length; j++)
+			{
+				for (int i = 0; i < input[0].Length; i++)
+				{
+					float result = 0.0f;
+
+					for (int n = 0; n < weight.Length; n++)
+					{
+						int index = j + n - (weight.Length - 1) / 2;
+						float value = input[Math.Max(0, Math.Min(index, input.Length - 1))][i];
+						result += value * weight[n];
+					}
+
+					output[j][i] = result;
+				}
+			}
+
+			File.WriteAllText("output.txt", string.Join("\r\n", output.Select(x => string.Join("\t", x))));
+		}
+
+		static IEnumerable<float> GetWeights(int taps)
+		{
+			var weights = Gaussian().ToArray();
+			var sum = weights.Sum();
+			return weights.Select(x => x / sum).ToArray();
+
+			IEnumerable<float> Gaussian()
+			{
+				for (int t = 0; t < taps; t++)
+				{
+					var x = (t - (taps - 1) / 2.0f) / ((taps - 1) / 5.0f);
+					var weight = (float)(Math.Exp(-0.5f * x * x) / taps);
+					yield return weight;
+				}
+			}
+		}
+	}
+}
