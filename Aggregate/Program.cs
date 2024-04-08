801deb9e27f36a4948db292611514a04d83e15e5
Tigrou Ind <tigrou.ind@gmail.com>
2024-04-08 10:58:51 +0200

    Initial commit

---
+++ Aggregate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Aggregate
+{
+	class Program
+	{
+		public static void Main()
+		{
+			TimeSpan average = TimeSpan.FromMinutes(1);
+			Func<DateTime, DateTime> round = x => new DateTime((x.Ticks / average.Ticks) * average.Ticks);
+
+			var lines = System.IO.File.ReadAllLines("input.txt")
+				.Select(x => x.Replace(",", "."))
+				.Where(x => !string.IsNullOrEmpty(x))
+				.ToArray();
+
+			var hasHeader = lines.Any() && Regex.IsMatch(lines[0], @"[^0-9\.\s]");
+			var header = lines.Take(hasHeader ? 1 : 0);
+			var rows = lines.Skip(1).ToArray();
+
+			System.IO.File.WriteAllLines(@"output.txt", header.Concat(rows
+										 .Select(x => x.Split('\t'))
+										 .Select(x => new Tuple<DateTime, float[]>(round(DateTime.Parse(x[0])), x.Skip(1).Select(y => float.Parse(y)).ToArray()))
+										 .GroupBy(x => x.Item1, x => x.Item2)
+										 .Select(x => x.Key.ToString("HH:mm") + "\t" + string.Join("\t", Scale(Transpose(x.ToArray())).Select(y => y.Max())))));
+		}
+
+		static float[][] Scale(float[][] input)
+		{
+			var largeValue = input.Select(row => row.Any(x => x >= 10000)).ToArray();
+			return Enumerable.Range(0, input.Length)
+				.Select(row => largeValue[row] ? input[row].Select(x => x / (1024 * 1024)).ToArray() : input[row])
+				.ToArray();
+		}
+
+		static float[][] Transpose(float[][] input)
+		{
+			int columns = input.Min(row => row.Length);
+			return Enumerable.Range(0, columns)
+				.Select(col => input.Select(row => row[col]).ToArray())
+				.ToArray();
+		}
+	}
+}
---
+++ Aggregate/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aggregate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aggregate")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cc2d3e70-467b-42f8-840b-d3fe3a24a605")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
